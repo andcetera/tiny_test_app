148cf0a79c1ad38d5358969d4871ba18a89c5113
BeckyK <barimonsterbk@gmail.com>
2023-02-15 15:01:31 -0600

    what I've got so far

---
+++ app.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, jsonify, render_template
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import psycopg2
+import json
+
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/Malaysia')
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Roadacc = Base.classes.road_accidents
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ProcessUserinfo/<string:userinfo>', methods=['POST'])
+def ProcessUserinfo(userinfo):
+    userinfo = json.loads(userinfo)
+    username = userinfo
+    print('-----')
+    print(f"Your name is {username}")
+    print('-----')
+    return('/')
+
+@app.route('/accidents')
+def accidents():
+    session = Session(engine)
+    data = session.query(Roadacc.road_crashes, Roadacc.population, Roadacc.year).all()
+    session.close()
+
+    results = [{"Crashes": r.road_crashes, "Population": r.population, "Year": r.year} for r in data]
+
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,31 @@
+url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+
+d3.json(url).then(function(data){
+    console.log(data);
+
+    let title = data.samples[0].otu_labels[0].split(';')
+    console.log(title);
+    d3.select('h1').text(title[3]);
+
+    for(i=0;i<title.length;i++){
+        d3.select('ul').append('li').text(title[i]);
+    }
+
+});
+
+function doStuff(){
+    console.log('getting name...');
+    let userinfo = document.getElementById('username').value;
+    console.log(`Hello, ${userinfo}`);
+    const request = new XMLHttpRequest();
+    request.open('POST', `/ProcessUserinfo/${JSON.stringify(userinfo)}`);
+    request.send();
+}
+
+function getData(){
+    console.log('getting data...');
+    url = 'http://127.0.0.1:5000/accidents'
+    fetch(url).then(response => response.json())
+    .then(json => {console.log(json);
+    document.getElementById('test').innerHTML = JSON.stringify(json)});
+}
