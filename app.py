cc15184d99f9b4edab0d5a7b301c5f5a52b1c6ad
BeckyK <barimonsterbk@gmail.com>
2023-02-16 12:51:23 -0600

    fix mistakes

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 def getYear(start):
     year = int(start)
     session = Session(engine)
-    data = session.query(Roadacc.road_crashes, Roadacc.year).filter(Roadacc > year).all()
+    data = session.query(Roadacc.road_crashes, Roadacc.year).filter(Roadacc.year > year).all()
     session.close()
-    results = [{"Crashes": r.road_crashes}, "Year": r.year} for r in data]
+    results = [{"Crashes": r.road_crashes, "Year": r.year} for r in data]
     return jsonify(results)
